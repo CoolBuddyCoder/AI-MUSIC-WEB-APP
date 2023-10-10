8f72b903aebb698bbea0ad7c139be170f408f8e8
CoolBuddyCoder <143184740+CoolBuddyCoder@users.noreply.github.com>
2023-10-10 15:50:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ leftWristX = 0;
 leftWristY = 0;
 RightWristX = 0;
 RightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 function setup() {
     canvas = createCanvas(600,500);
     canvas.center();
@@ -20,6 +22,8 @@ function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
         scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
         console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -34,6 +38,34 @@ function draw() {
     image(video, 0, 0, 600, 500);
     fill("#FF000");
     stroke("#FF000");
+
+    if (scoreRightWrist > 0.2) {
+        circle(RightWristX,RightWristY,20);
+    if (RightWristY > 0 && RightWristY<= 100) {
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if (RightWristY > 100 && RightWristY <= 200) {
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if (RightWristY > 200 && RightWristY <= 300) {
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if (RightWristY > 300 && RightWristY <= 400) {
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else if (RightWristY > 400 && RightWristY <= 500) {
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5);
+    }
+    }
+    
+
+
+
     if (scoreLeftWrist > 0.2) 
     {
         circle(leftWristX, leftWristY,20);
